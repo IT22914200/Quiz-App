245723bfe15feff888e071b5188023de65faf0e1
Tharushika W.A.M <it22914200@my.sliit.lk>
2025-06-23 23:45:01 +0530

    Update Quiz.jsx
    Update Quiz.jsx

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,1 +1,23 @@
-.j
+import React from 'react'
+import './Quiz.css'
+
+const Quiz = () => {
+  return (
+    <div className='container'>
+        <h1>Quiz App</h1>
+        <hr />
+        <h2>Whice device is required dor the Internet connection </h2>
+        <ul>
+            <li>Modem</li>
+            <li>Router</li>
+            <li>LAN Cable</li>
+            <li>Pen Drive</li>
+        </ul>
+        <button>Next</button>
+        <div className="index">1 of 5 questions</div>
+
+    </div>
+  )
+}
+
+export default Quiz
